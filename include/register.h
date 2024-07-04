7419b2d77bc5111aa6f3a4a71640d021750f1cbf
kmugen <khj199859@gmail.com>
2024-07-04 19:33:25 +0900

    Add macros for bit write
    Modified files:
        - main.cpp (testcode for macros)
        - register.h (containing new macros
    
    Added macros (register.h):
        - SET_BIT(addr, bit): set bit as '1' at n'th bit from base address (addr)
        - CLR_BIT(addr, bit): clear bit as '0' at n'th bit from base address (addr)
        - WRITE_BIT(addr, bit, val) : Write 'val' to a bit from base address (addr)
    
    Date: 24.07.04 19:33

---
+++ include/register.h
@@ -0,0 +1,8 @@
+#ifndef __REGISTER_H__
+#define __REGISTER_H__
+
+#define SET_BIT(addr, bit)          (*(addr) |= (0x1UL << (bit)))
+#define CLR_BIT(addr, bit)          (*(addr) &= ~(0x1UL << (bit)))
+#define WRITE_BIT(addr, bit, val)   ((val) ? SET_BIT(addr, bit) : CLR_BIT(addr, bit))
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
+#include "register.h"
+
+volatile uint8_t *ddrb = (volatile uint8_t *)0x23;
+volatile uint8_t *portb = (volatile uint8_t *)0x24;
 
 void setup()
 {
+    // set portb pin 4, 5 as digital out
+    WRITE_BIT(ddrb, 4, 1);
+    WRITE_BIT(ddrb, 5, 1);
+
+    // set led on
+    WRITE_BIT(portb, 4, 1);
+    WRITE_BIT(portb, 5, 1);
+
 }
 
 void loop()
