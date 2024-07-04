1ee2de09b31c7a5758a234be868fc0d53160dfa5
kmugen <khj199859@gmail.com>
2024-07-04 20:25:06 +0900

    Add #define macros for gpio register address
    Modified Files:
        - gpio_reg.h
            - Add #define macros for PIN, DDR, PORT for port B, C, D
        - main.cpp
            - Turn leds on using gpio macros (previous was using hard coding)
    
    Date: 24.07.04 20:25

---
+++ include/gpio_reg.h
@@ -0,0 +1,34 @@
+#ifndef __GPIO_REG_H__
+#define __GPIO_REG_H__
+
+#include <inttypes.h>
+
+#define BASE_GPIO           ((volatile uint8_t *) 0x23UL)
+
+#define SIZE_GPIO_REG       (0x03UL)
+
+#define IDX_B               (0x00UL)
+#define IDX_C               (0x01UL)
+#define IDX_D               (0x02UL)
+
+#define IDX_PIN             (0x00UL)
+#define IDX_DDR             (0x01UL)
+#define IDX_PORT            (0x02UL)
+
+#define BASE_B              ((BASE_GPIO) + ((SIZE_GPIO_REG) * (IDX_B)))
+#define BASE_C              ((BASE_GPIO) + ((SIZE_GPIO_REG) * (IDX_C)))
+#define BASE_D              ((BASE_GPIO) + ((SIZE_GPIO_REG) * (IDX_D)))
+
+#define PIN_B               ((BASE_B) + (IDX_PIN))
+#define DDR_B               ((BASE_B) + (IDX_DDR))
+#define PORT_B              ((BASE_B) + (IDX_PORT))
+
+#define PIN_C               ((BASE_C) + (IDX_PIN))
+#define DDR_C               ((BASE_C) + (IDX_DDR))
+#define PORT_C              ((BASE_C) + (IDX_PORT))
+
+#define PIN_D               ((BASE_D) + (IDX_PIN))
+#define DDR_D               ((BASE_D) + (IDX_DDR))
+#define PORT_D              ((BASE_D) + (IDX_PORT))
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 #include "register.h"
-
-volatile uint8_t *ddrb = (volatile uint8_t *)0x23;
-volatile uint8_t *portb = (volatile uint8_t *)0x24;
+#include "gpio_reg.h"
 
 void setup()
 {
     // set portb pin 4, 5 as digital out
-    WRITE_BIT(ddrb, 4, 1);
-    WRITE_BIT(ddrb, 5, 1);
+    WRITE_BIT(DDR_B, 4, 1);
+    WRITE_BIT(DDR_B, 5, 1);
 
     // set led on
-    WRITE_BIT(portb, 4, 1);
-    WRITE_BIT(portb, 5, 1);
+    WRITE_BIT(PORT_B, 4, 1);
+    WRITE_BIT(PORT_B, 5, 1);
 
 }
 
