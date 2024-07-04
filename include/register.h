428d14822435f3bafeb23d94ba6e43163e8c4af0
kmugen <khj199859@gmail.com>
2024-07-04 20:42:26 +0900

    Add type definition for register
    Modified Files:
        - register.h
            - reg_t: volatile uint8_t
    
    Date: 24.07.04 20:42

--- include/register.h
+++ include/register.h
@@ -5,4 +5,6 @@
 #define CLR_BIT(addr, bit)          (*(addr) &= ~(0x1UL << (bit)))
 #define WRITE_BIT(addr, bit, val)   ((val) ? SET_BIT(addr, bit) : CLR_BIT(addr, bit))
 
+typedef reg_t                       volatile uint8_t;
+
 #endif
