d5b03dad4e9726fc577da46a07c25f379dd09969
kmugen <khj199859@gmail.com>
2024-07-06 04:21:04 +0900

    Add feature for setting output state
    Add function that set a pin's digital output state as HIGH/LOW
    
    Date: 24.07.06 04:21

--- lib/Gpio/gpio.cpp
+++ lib/Gpio/gpio.cpp
@@ -32,4 +32,31 @@ bool SetPinMode(reg_t *port, uint8_t pin, uint8_t mode)
     return ret_status;
 }
 
+bool SetPinState(reg_t *port, uint8_t pin, uint8_t state)
+{
+    bool ret_status = NO_ERR;
+
+    if ((port != BASE_B) && (port != BASE_C) && (port != BASE_D))
+    {
+        ret_status = ERR;
+        return ret_status;
+    }
+
+    if ((port == BASE_C) && (pin == 7))
+    {
+        ret_status = ERR;
+        return ret_status;
+    }
+
+    if ((state != HIGH) && (state != LOW))
+    {
+        ret_status = ERR;
+        return ret_status;
+    }
+
+    WRITE_BIT(port, pin, state);
+
+    return ret_status;
+}
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ void setup()
 {
     // set portb pin 4, 5 as digital out
     SetPinOuput(BASE_B, 5);
-    SET_BIT(PORT(BASE_B), 5);
+    SetPinState(BASE_B, 5, HIGH);
     // set led on
 
 }
